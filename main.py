ebc96db9d07dc9964ab46ec8c1c473be89b3ad9c
stevekama <stevekamahertz@gmail.com>
2019-08-07 17:02:44 +0300

    Login with social app

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return "Hello world"
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
