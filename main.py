504eee113b656ea57c507ace24a5fcd47a1a649e
stevekama <stevekamahertz@gmail.com>
2019-08-08 17:28:42 +0300

    dashboard

--- main.py
+++ main.py
@@ -9,5 +9,9 @@ def hello_world():
 def login():
     return render_template('login.html')
 
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
